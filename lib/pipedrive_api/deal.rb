6634edf7eaf7df2b673f7ca8ac4685d5c16d977d
Trond Michelsen <trond@bankinspain.com>
2018-08-21 22:15:50 +0200

    Add a deal

--- lib/pipedrive_api/deal.rb
+++ lib/pipedrive_api/deal.rb
@@ -6,6 +6,12 @@ module PipedriveAPI
         response = post "#{resource_path}/#{id}/duplicate"
         handle response
       end
+      
+      def add_a_deal
+        response = post resource_path, body: params.to_json
+        handle response
+      end
+      
     end
   end
 end
